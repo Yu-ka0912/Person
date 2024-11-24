1c5072ced46dc3d255425d612efc12ef1068a2db
Yu-ka0912 <aoyamayuka.0912@gmail.com>
2024-11-24 23:01:41 +0900

    インスタンス化

--- src/jp/co/diworks/action/Test.java
+++ src/jp/co/diworks/action/Test.java
@@ -1,5 +1,53 @@
 package jp.co.diworks.action;
 
 public class Test {
-
+	public static void main(String[]args){
+		Person taro=new Person();
+		taro.name="山田太郎";
+		taro.age=20;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		
+		//木村次郎１８歳
+		Person jirou=new Person();
+		jirou.name="木村次郎";
+		jirou.age=18;
+		
+		System.out.println(jirou.name);
+		System.out.println(jirou.age);
+		
+		//鈴木花子１６歳
+		Person hanako=new Person();
+		hanako.name="鈴木花子";
+		hanako.age=16;
+		
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+		
+		//自分な名前、年齢
+		Person yuka=new Person();
+		yuka.name="青山友香";
+		yuka.age=24;
+		
+		System.out.println(yuka.name);
+		System.out.println(yuka.age);
+		
+		//田中三郎１０歳,03-1234-5678,東京都
+		Person saburou=new Person();
+		saburou.name="田中三郎";
+		saburou.age=10;
+		saburou.phoneNumber="03-1234-5678";
+		saburou.address="東京都";
+		
+		System.out.println(saburou.name);
+		System.out.println(saburou.age);
+		System.out.println(saburou.phoneNumber);
+		System.out.println(saburou.address);
+		
+		//演習５、６
+		taro.talk();
+		taro.walk();
+		taro.run();
+	}
 }
